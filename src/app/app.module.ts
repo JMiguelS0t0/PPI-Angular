826de0b07357f59f683f269590d74e7647c77725
Juan Miguel <juanmiguelsoto4@gmail.com>
2023-10-09 14:36:36 -0500

    Primeros archivos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { RouterModule, Routes } from '@angular/router';
 
+// const APP_ROUTES: Routes = [
+//   {
+//     path: 'Inicio',
+//     loadChildren: () =>
+//       import('./inicio/inicio.module').then((m) => m.InicioModule),
+//   },
+//   {
+//     path: '**',
+//     pathMatch: 'full',
+//     redirectTo: 'inicio',
+//   },
+// ];
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, InicioComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
