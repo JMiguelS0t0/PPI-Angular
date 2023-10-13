082c7748e3a62e78a90d843513cd603bd559ac85
Juan Miguel <juanmiguelsoto4@gmail.com>
2023-10-13 01:22:12 -0500

    Routing y ServiciosPrestados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InicioComponent } from './inicio/inicio.component';
+import { ServiciosPrestadosComponent } from './servicios-prestados/servicios-prestados.component';
+import { SharedModule } from './shared/shared.module';
 import { RouterModule, Routes } from '@angular/router';
 
-// const APP_ROUTES: Routes = [
-//   {
-//     path: 'Inicio',
-//     loadChildren: () =>
-//       import('./inicio/inicio.module').then((m) => m.InicioModule),
-//   },
-//   {
-//     path: '**',
-//     pathMatch: 'full',
-//     redirectTo: 'inicio',
-//   },
-// ];
+const APP_ROUTES: Routes = [
+  {
+    path: 'inicio',
+    component: InicioComponent,
+  },
+  {
+    path: 'serviciosprestados',
+    component: ServiciosPrestadosComponent,
+  },
+  {
+    path: '**',
+    pathMatch: 'full',
+    redirectTo: 'inicio',
+  },
+];
+
 @NgModule({
-  declarations: [AppComponent, InicioComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [AppComponent, InicioComponent, ServiciosPrestadosComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    SharedModule,
+    RouterModule.forRoot(APP_ROUTES),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/app/servicios-prestados/servicios-prestados.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-servicios-prestados',
+  templateUrl: './servicios-prestados.component.html',
+  styleUrls: ['./servicios-prestados.component.css']
+})
+export class ServiciosPrestadosComponent {
+
+}
