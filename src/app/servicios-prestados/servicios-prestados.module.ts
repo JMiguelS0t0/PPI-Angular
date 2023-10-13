1ab0533d831d6be1d45027b760d4608a7ed84f6f
Juan Miguel <juanmiguelsoto4@gmail.com>
2023-10-13 15:58:27 -0500

    DetalleServicios (Componente)

--- src/app/servicios-prestados/servicios-prestados.module.ts
+++ src/app/servicios-prestados/servicios-prestados.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
+import { DetalleServiciosComponent } from './detalle-servicios/detalle-servicios.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    DetalleServiciosComponent
+  ],
   imports: [SharedModule],
 })
 export class ServiciosprestadosModule {}
